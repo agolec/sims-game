2f0a6b2f8d5393e141b7f83663bb9c09fb0fc00e
Adam <algolec1990@yahoo.com>
2023-12-19 13:17:08 -0500

    modify beginnings of scheduleClassTests to reflect changes in the Schedule.java class file.

--- test/time/scheduleClassTests/scheduleClassTests.java
+++ test/time/scheduleClassTests/scheduleClassTests.java
@@ -1,2 +1,16 @@
-package time.scheduleClassTests;public class scheduleClassTests {
+package time.scheduleClassTests;
+
+import org.testng.annotations.Test;
+import time.Schedule;
+
+@Test
+public class scheduleClassTests {
+    public void test(){
+        Schedule playerSchedule = new Schedule();
+        playerSchedule.clearSchedule();
+        playerSchedule.setSchedule(3);
+
+        System.out.println(playerSchedule.getSchedule());
+    }
+
 }
