a28a2ad291d775814f7eac787e49a9c84a5437ad
Adam <algolec1990@yahoo.com>
2023-12-18 22:25:43 -0500

    modify tests to accomidate changes in the Schedule class.

--- test/time/scheduleClassTests/scheduleClassTests.java
+++ test/time/scheduleClassTests/scheduleClassTests.java
@@ -1,2 +1,26 @@
-package time.scheduleClassTests;public class scheduleClassTests {
+package time.scheduleClassTests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import time.DayOfWeek;
+import time.Schedule;
+
+@Test
+public class scheduleClassTests {
+    Schedule playerSchedule = new Schedule();
+    public void ScheduleClassTests(){
+        Assert.assertEquals(5,Schedule.WORK_DAYS_IN_WEEK);
+        Assert.assertEquals(7,Schedule.NUMBER_OF_DAYS_IN_WEEK);
+    }
+    public void assignNumberOfDays(){
+        System.out.println("assigning number of work days in the week");
+        playerSchedule.setNumberOfDaysWorking(3);
+        System.out.println("number of days working: " + playerSchedule.getNumberOfDaysWorking());
+        System.out.println("Generating schedule. . .");
+        playerSchedule.generateSchedule();
+        int[] daysWorking = playerSchedule.getSchedule();
+        for(int i = 0; i < daysWorking.length;i++){
+            System.out.print(daysWorking[i] + " ");
+        }
+    }
 }
