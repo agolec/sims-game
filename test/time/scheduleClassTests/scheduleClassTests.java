9f795827ecaef5852164d4d6eb4fca23a61c5a10
Adam <algolec1990@yahoo.com>
2023-12-19 13:59:04 -0500

    modify scheduleClassTests to account for changes made to the class

--- test/time/scheduleClassTests/scheduleClassTests.java
+++ test/time/scheduleClassTests/scheduleClassTests.java
@@ -1,16 +1,19 @@
 package time.scheduleClassTests;
 
 import org.testng.annotations.Test;
+import time.Calendar;
 import time.Schedule;
 
 @Test
 public class scheduleClassTests {
     public void test(){
-        Schedule playerSchedule = new Schedule();
+        Calendar cal = new Calendar();
+        Schedule playerSchedule = new Schedule(cal);
         playerSchedule.clearSchedule();
         playerSchedule.setSchedule(3);
 
         System.out.println(playerSchedule.getSchedule());
+        System.out.println(playerSchedule);
     }
 
 }
