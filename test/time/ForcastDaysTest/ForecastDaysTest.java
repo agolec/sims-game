a71574cd028187c0aa4d3b48f0ca4d85eed6e758
Adam <algolec1990@yahoo.com>
2023-12-21 14:49:06 -0500

    set up ForecastDaysTest.java class.

--- test/time/ForcastDaysTest/ForecastDaysTest.java
+++ test/time/ForcastDaysTest/ForecastDaysTest.java
@@ -1,2 +1,15 @@
-package time.ForcastDaysTest;public class ForecastDaysTest {
+package time.ForcastDaysTest;
+
+import org.testng.annotations.Test;
+import time.Calendar;
+import weather.ForcastDays;
+
+public class ForecastDaysTest {
+    Calendar cal = Calendar.getInstance();
+    ForcastDays forcast;
+    @Test
+    public void testDays(){
+        forcast = ForcastDays.getInstance(cal);
+        forcast.printList();
+    }
 }
