47d30b62ed59fc1bd8b846328d83d2ecdaac955c
Adam <algolec1990@yahoo.com>
2023-12-22 15:55:15 -0500

    begin to set up code to generate the weather.

--- src/weather/ForcastDays.java
+++ src/weather/ForcastDays.java
@@ -2,13 +2,18 @@ package weather;
 
 import time.Calendar;
 import time.DayOfWeek;
+import time.Month;
+import time.Season;
 
+import java.time.MonthDay;
 import java.util.List;
+import java.util.Random;
 
 public class ForcastDays {
     public static ForcastDays singleInstance;
     Calendar cal;
     List<DayOfWeek> nextThreeDays;
+    List <Weather> weatherForDays;
     final int DAYS_OF_FORCAST = 3;
     private ForcastDays(Calendar cal){
         this.cal = Calendar.getInstance();
@@ -19,6 +24,26 @@ public class ForcastDays {
             singleInstance = new ForcastDays(cal);
         }
         return singleInstance;
+    }
+    public List<Weather> generateWeather(){
+        int[] probabilities = new int[DAYS_OF_FORCAST];
+        probabilities = generateProbabilities(DAYS_OF_FORCAST);
+
+
+    }
+    private int[] generateProbabilities(int size){
+//        int[] probabilities = new int[size];
+//        Random random = new Random();
+//        for(int i = 0; i < size;){
+//            switch(cal.getCurrentSeason()){
+//                case SPRING:
+//                    probabilities[probabilities.length] = random.nextInt(0,500);
+//                    brea
+//
+//            }
+//        }
+        return new int [0];
+
     }
     public void printList(){
         System.out.println(nextThreeDays);
