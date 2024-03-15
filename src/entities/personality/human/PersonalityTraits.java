e9c8b70f38b9b38e28d433c2bcff5f46bd66fe13
Adam <algolec1990@yahoo.com>
2024-03-14 22:57:43 -0400

    add enums that are related to sims personality.

--- src/entities/personality/human/PersonalityTraits.java
+++ src/entities/personality/human/PersonalityTraits.java
@@ -1,2 +1,8 @@
-package entities.personality.human;public enum PersonalityTraits {
+package entities.personality.human;
+
+public enum PersonalityTraits {
+    LAZINESS_VS_ACTIVE,
+    SHYNESS_VS_OUTGOING,
+    NEATNESS_VS_MESSINESS,
+    PLAYFULNESS_SERIOUSNESS,
 }
