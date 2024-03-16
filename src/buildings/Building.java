394f7f354d713da3423e9a02fe3759d43f2dd992
Adam <algolec1990@yahoo.com>
2024-03-15 21:33:07 -0400

    rename method that throws an illegal argument exception for blank or null fields. Create the addRoom, deleteRoom, addFloor, deleteFloor functions. Create methods to throw illegal argument exceptions for invalid floor index, and invalid room index.

--- src/buildings/Building.java
+++ src/buildings/Building.java
@@ -1,19 +1,25 @@
 package buildings;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Building {
     private String name;
     private String address;
     private PropertyType propertyType;
+    private List<List<Room>> floors;
 
     public Building(String name, String address, PropertyType propertyType){
         setName(name);
         setAddress(address);
         setPropertyType(propertyType);
+        floors = new ArrayList<>();
     }
     public Building(Building source){
         this.setName(source.getName());
         this.setAddress(source.getAddress());
         this.setPropertyType(source.getPropertyType());
+        this.addFloor();
     }
 
     public String getName() {
@@ -30,14 +36,14 @@ public class Building {
 
     public void setName(String name) {
         if(name == null || name.isBlank()){
-            illegalArgumentThrow(name);
+            throwIllegalArgumentExceptionForNullOrBlankField(name);
         }
         this.name = name;
     }
 
     public void setAddress(String address) {
         if(address == null|| address.isBlank()){
-            illegalArgumentThrow(address);
+            throwIllegalArgumentExceptionForNullOrBlankField(address);
         }
         this.address = address;
     }
@@ -45,11 +51,40 @@ public class Building {
     public void setPropertyType(PropertyType propertyType) {
         String fieldName = "Property type";
         if(propertyType == null){
-            illegalArgumentThrow(fieldName);
+            throwIllegalArgumentExceptionForNullOrBlankField(fieldName);
         }
         this.propertyType = propertyType;
     }
-    public void illegalArgumentThrow(String fieldName){
+    public void addRoom(int floorIndex, Room room){
+        if(floorIndex < 0 || floorIndex >= this.floors.size()){
+            throwIllegalArgumentExceptionForInvalidFloorIndex();
+        }
+    }
+    public void deleteRoom(int floorIndex, int roomIndex){
+        if(floorIndex < 0 || floorIndex >= this.floors.size()){
+            throwIllegalArgumentExceptionForInvalidFloorIndex();
+        }
+        if(roomIndex < 0 || roomIndex >= floors.get(floorIndex).size()){
+            throwIllegalArgumentExceptionForInvalidRoomIndex();
+        }
+        floors.get(floorIndex).remove(roomIndex);
+    }
+    public void addFloor(){
+        this.floors.add(new ArrayList<>());
+    }
+    public void deleteFloor(int floorIndex){
+        if(floorIndex < 0 || floorIndex >= floors.size()){
+            throwIllegalArgumentExceptionForInvalidFloorIndex();
+        }
+        floors.remove(floorIndex);
+    }
+    public void throwIllegalArgumentExceptionForNullOrBlankField(String fieldName){
         throw new IllegalArgumentException(fieldName + " cannot be null or blank.");
+    }
+    public void throwIllegalArgumentExceptionForInvalidFloorIndex(){
+        throw new IllegalArgumentException("Invalid floor index.");
+    }
+    public void throwIllegalArgumentExceptionForInvalidRoomIndex(){
+        throw new IllegalArgumentException("Invalid room index.");
     }
 }
