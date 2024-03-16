052801cfc89fb7eccd81c6f0410a292a8daedc28
Adam <algolec1990@yahoo.com>
2024-03-15 21:12:17 -0400

    add javadocs in to PropertyType.java.

--- src/buildings/PropertyType.java
+++ src/buildings/PropertyType.java
@@ -1,5 +1,8 @@
 package buildings;
 
+/**
+ * PropetyType defines the enums for a property, whether it is residential, commercial, or industrial.
+ */
 public enum PropertyType {
     RESIDENTIAL,
     COMMERCIAL,
