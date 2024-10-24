87bf3038591c90906a1418253394ae95c90db2ac
Adam <algolec1990@yahoo.com>
2024-10-24 09:24:22 -0400

    set up initial game engine.

--- src/engine/GameEngine.java
+++ src/engine/GameEngine.java
@@ -1,2 +1,18 @@
-package engine;public class GameEngine {
+package engine;
+
+import entities.NonPlayerCharacter;
+import entities.PlayerCharacter;
+
+import java.util.ArrayList;
+
+public class GameEngine {
+    boolean isRunning;
+    private PlayerCharacter player;
+    private ArrayList<NonPlayerCharacter> nonPlayerCharacters;
+
+    public GameEngine(){
+        isRunning = true;
+        this.player = new PlayerCharacter();
+
+    }
 }
