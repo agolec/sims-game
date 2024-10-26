087b2e31bb49e8d47a80890ddb832b1e28923cf5
Adam <algolec1990@yahoo.com>
2024-10-26 10:22:21 -0400

    Remove Needs and PersonalityTraits java classes from Entity package. These do not belong here.

---
+++ test/Entity/EntityClassTests/Sim/entityClassTests.java
@@ -0,0 +1,12 @@
+package Entity.EntityClassTests.Sim;
+
+import exceptions.InvalidEntityNameException;
+import org.testng.annotations.Test;
+import entities.Entity;
+
+@Test
+public class entityClassTests {
+    public void assertEntityCreatesSuccessfullyWhenAssignedValidName() throws InvalidEntityNameException {
+
+    }
+}
