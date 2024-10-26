069f2239b0cbea61aedaf93570219a25ffe93cb3
Adam <algolec1990@yahoo.com>
2024-10-26 00:24:58 -0400

    Remove all un-used imports. Set up Entity class to be incredibly basic for initial MVP.
    Create InvalidEntityNameException class so name cannot be blank. idNumber will generate whenever constructor is called.

--- src/entities/Entity.java
+++ src/entities/Entity.java
@@ -1,16 +1,33 @@
 package entities;
 
-import entities.needs.human.Needs;
-import entities.personality.human.PersonalityTraits;
-
-import java.util.HashMap;
-import java.util.Map;
+import exceptions.InvalidEntityNameException;
 
 public abstract class Entity {
     private String name;
+    private int idNumber;
 
-    public Entity(){
-
+    public Entity(String name) throws InvalidEntityNameException {
+        this.generateId();
+        if(name.isBlank() || name.isEmpty()){
+            throw new InvalidEntityNameException("Error: Entity name cannot be blank or empty.");
+        }
     }
+    private void generateId(){
+        idNumber++;
+    }
+    public int getId(){
+        return this.idNumber;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public String getString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("ID: " + this.getId() + "\n");
+        sb.append("Name: " + this.getName() + "\n");
+        return sb.toString();
+    }
+
+
 
 }
