cce62992fad9ef2130001b71b5a8ec06e66992bb
Adam Golec <algolec1990@yahoo.com>
2024-10-27 11:55:02 -0400

    Create Base Class for Item, and Enum describing ItemType.

---
+++ src/Item/Item.java
@@ -0,0 +1,40 @@
+package Item;
+
+import entities.Entity;
+import exceptions.InvalidEntityNameException;
+
+/**
+ * The Item class is meant to be the base class for any implementation of an item.
+ */
+public abstract class Item extends Entity {
+    private ItemType type;
+    private double price;
+    public Item(String name, ItemType type) throws InvalidEntityNameException {
+        super(name);
+        this.type = type;
+    }
+    public abstract void use();
+    public void setType(ItemType type){
+        this.type = type;
+    }
+    public void setPrice(Double price){
+        this.price = price;
+    }
+    public ItemType getType(){
+        return this.type;
+    }
+    public Double getPrice(){
+        return this.price;
+    }
+    public String toString(){
+
+        String itemString = "ID: " + super.getId() + "\n" +
+                "Name: " + super.getName() + "\n" +
+                "Description" + this.getType() + "\n" +
+                "Price: $" + this.getPrice();
+
+        return itemString;
+
+    }
+
+}
