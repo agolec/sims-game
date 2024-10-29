2401196d8c1b64766e0959e2465fe7129accafdb
Adam Golec <algolec1990@yahoo.com>
2024-10-29 11:25:06 -0400

    Create Utility classes for Entity and Sim.

--- src/Item/Item.java
+++ src/Item/Item.java
@@ -1,29 +1,47 @@
 package Item;
 
 import entities.Entity;
+import entities.Sim;
 import exceptions.InvalidEntityNameException;
 import exceptions.InvalidItemPriceException;
+import utils.EntityUtils;
+import utils.SimUtils;
 
 /**
  * The Item class is meant to be the base class for any implementation of an item.
  */
 public abstract class Item extends Entity {
     private ItemType type;
+    private Sim owner;
     private double price;
     private String description;
-    public Item(String name, ItemType type, Double price, String description) throws InvalidEntityNameException, InvalidItemPriceException {
+    public Item(String name, ItemType type, Sim owner, Double price, String description) throws InvalidEntityNameException, InvalidItemPriceException {
         super(name);
+        this.setType(type);
+        this.setOwner(owner);
         this.setPrice(price);
         this.setDescription(description);
     }
-    public Item(String name, ItemType type, Double price) throws InvalidEntityNameException, InvalidItemPriceException {
+    public Item(String name, ItemType type, Sim owner, Double price) throws InvalidEntityNameException, InvalidItemPriceException {
         super(name);
+        this.setType(type);
+        this.setOwner(owner);
         this.setPrice(price);
         this.setDescription("");
     }
     public abstract void use();
+    public abstract Item addToInventory();
     public void setType(ItemType type){
         this.type = type;
+    }
+
+    /**
+     *
+     * @param intendedOwner - The sim the Item is trying to apply ownership to.
+     * @throws InvalidEntityNameException - If Sim Entity does not have a name assigned to them, this will be thrown
+     */
+    public void setOwner(Sim intendedOwner) throws InvalidEntityNameException {
+        this.owner = (SimUtils.simExists(intendedOwner)) ? new Sim(intendedOwner): null;
     }
     public void setPrice(Double price) throws InvalidItemPriceException {
         if(this.price < 0){
@@ -42,11 +60,17 @@ public abstract class Item extends Entity {
     public ItemType getType(){
         return this.type;
     }
+    public Sim getOwner(){
+        return this.owner;
+    }
     public Double getPrice(){
         return this.price;
     }
     public String getDescription(){
         return this.description;
+    }
+    public boolean hasOwner(){
+        return SimUtils.simExists(this.owner);
     }
     public String toString(){
 
