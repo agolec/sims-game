41f711e885e10bcd6008e2ebad3d51a0659ce6e3
Adam Golec <algolec1990@yahoo.com>
2024-11-25 11:55:20 -0500

    implement InventoryHolder on Sim to create contract for inventory containment on Sim class.

--- src/entities/Sim.java
+++ src/entities/Sim.java
@@ -1,12 +1,36 @@
 package entities;
 
 import exceptions.InvalidEntityNameException;
+import inventory.Inventory;
+import inventory.InventoryHolder;
+import item.Item;
 
-public class Sim extends Entity {
+public class Sim extends Entity implements InventoryHolder {
+
+    private Inventory inventory;
     public Sim(String name) throws InvalidEntityNameException {
         super(name);
     }
+    public Sim(String name, Inventory inventory) throws InvalidEntityNameException{
+        super(name);
+        this.inventory = inventory;
+    }
     public Sim(Sim sourceSim) throws InvalidEntityNameException{
         super(sourceSim.getName());
+    }
+
+    @Override
+    public Inventory getInventory() {
+        return this.inventory;
+    }
+
+    @Override
+    public void addItemToInventory(Item item, int quantity) {
+        this.inventory.addItem(item,quantity);
+    }
+
+    @Override
+    public void removeItemFromInventory(Item item, int quantity) {
+        this.inventory.removeItem(item,quantity);
     }
 }
