8c069ee32caf60dd23bf8c03c429aba407c22252
Adam Golec <algolec1990@yahoo.com>
2024-11-25 00:50:29 -0500

    make new exception for itemquanity when trying to set it under 0.

--- src/exceptions/InvalidItemQuantityException.java
+++ src/exceptions/InvalidItemQuantityException.java
@@ -1,2 +1,7 @@
-package exceptions;public class InvalidItemQuantityException {
+package exceptions;
+
+public class InvalidItemQuantityException extends Exception {
+    public InvalidItemQuantityException(String message){
+        super(message);
+    }
 }
