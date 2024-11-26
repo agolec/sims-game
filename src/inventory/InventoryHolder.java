3bc1bc788a177e3f828850e80b3f2719ebbc7446
Adam Golec <algolec1990@yahoo.com>
2024-11-26 10:42:01 -0500

    Add method getInventorySize() to InventoryHolder interface.

--- src/inventory/InventoryHolder.java
+++ src/inventory/InventoryHolder.java
@@ -6,4 +6,5 @@ public interface InventoryHolder {
     Inventory getInventory();
     void addItemToInventory(Item item, int quantity);
     void removeItemFromInventory(Item item, int quantity);
+    int getInventorySize();
 }
