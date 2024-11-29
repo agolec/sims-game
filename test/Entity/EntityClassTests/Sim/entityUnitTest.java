5ec8f672dbca57fd5dfb31c13a694b5864506c7f
Adam Golec <algolec1990@yahoo.com>
2024-11-29 00:21:54 -0500

    change text in thrown exception InvalidEntityNameException to include information that name cannot be null.

--- test/Entity/EntityClassTests/Sim/entityUnitTest.java
+++ test/Entity/EntityClassTests/Sim/entityUnitTest.java
@@ -10,23 +10,21 @@ import static org.junit.Assert.assertThrows;
 
 public class entityClassTests {
     final String VALID_ENTITY_NAME = "Ted";
-    final String EMPTY_STRING_NAME = "";
-    final String NULL_STRING = null;
-    final int ZERO = 0;
+    private final String EXPECTED_EXCEPTION_MESSAGE_TEXT = "Error: Entity name cannot be blank, null, or empty.";
     @Test
     public void assertEntityCreatesSuccessfullyWhenAssignedValidName() throws InvalidEntityNameException {
 
         Sim testSim = new Sim(VALID_ENTITY_NAME);
         Assert.assertNotNull("Expected Sim object to be created, but it was null",testSim);
         assertEquals(testSim.getName(), VALID_ENTITY_NAME,testSim.getName());
-        Assert.assertTrue("Expected ID to be greater than 0, but it was equal to or less than 0",testSim.getId() > ZERO);
+        Assert.assertTrue("Expected ID to be greater than 0, but it was equal to or less than 0",testSim.getId() > 0);
 }
 @Test
     public void assertSimThrowsExceptionWhenNameBlank()  {
         try{
-            Sim sim = new Sim(EMPTY_STRING_NAME);
+            Sim sim = new Sim("");
         } catch (InvalidEntityNameException e){
-            assertEquals("Error: Entity name cannot be blank or empty.", e.getMessage());
+            assertEquals(EXPECTED_EXCEPTION_MESSAGE_TEXT, e.getMessage());
         }
     }
     @Test()
@@ -34,7 +32,7 @@ public class entityClassTests {
         try{
             Sim testSim = new Sim((String) null);
         } catch (Exception e){
-            assertEquals("Error: Entity name cannot be blank or empty.", e.getMessage());
+            assertEquals(EXPECTED_EXCEPTION_MESSAGE_TEXT, e.getMessage());
         }
     }
 
