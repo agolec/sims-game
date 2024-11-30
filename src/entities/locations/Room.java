f6a8a9aa29474096ef32400674d567c728fb6e25
Adam Golec <algolec1990@yahoo.com>
2024-11-30 12:02:13 -0500

    create room class extending off Location abstract class.

--- src/entities/locations/Room.java
+++ src/entities/locations/Room.java
@@ -1,2 +1,14 @@
-package entities.locations;public class Room {
+package entities.locations;
+
+import entities.Entity;
+import entities.base.Location;
+
+import java.util.ArrayList;
+
+public class Room  extends Location {
+    ArrayList<Entity> entitiesInRoom;
+    public Room(String name, String description, ArrayList<Location> connectingRooms){
+        super(name,description,connectingRooms);
+        entitiesInRoom = new ArrayList<>();
+    }
 }
