3a6592e355879c5d2c47373e5a6320efb26166ef
Adam Golec <algolec1990@yahoo.com>
2024-12-05 11:30:48 -0500

    set up assertConsumableItemSuccessfullyCreated() to test consumable item creation.

--- test/item/itemUnitTests.java
+++ test/item/itemUnitTests.java
@@ -1,2 +1,36 @@
-package item;public class itemUnitTests {
+package item;
+
+import entities.Sim;
+import exceptions.InvalidEntityNameException;
+import inventory.Inventory;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.testng.annotations.BeforeTest;
+
+public class itemUnitTests {
+    private Item potatoChip;
+    private Sim sim;
+    private Inventory inventory;
+
+    @Before
+    public void setUp() throws InvalidEntityNameException {
+        try{
+            potatoChip = new FoodItem("Brotato Chips", null,"A bag of potato chips",false, 1);
+            inventory = new Inventory();
+            inventory.addItem(potatoChip,1);
+            sim = new Sim("John Doe",inventory);
+
+        } catch(InvalidEntityNameException e){
+            System.out.println(e.getMessage());
+        }
+        Assert.assertNotNull("Error: expected " + potatoChip.getClass().getSimpleName() + " to not be null, and it was null.",potatoChip);
+    }
+
+
+    @Test
+    public void assertConsumableFoodItemSuccessfullyCreated() throws InvalidEntityNameException {
+        Assert.assertEquals(1, this.sim.getInventorySize());
+
+    }
 }
