1f2b43422976f4bd94d76835f586bf726860f4a9
Adam Golec <algolec1990@yahoo.com>
2024-12-09 11:51:59 -0500

    modify toString method for this class.

--- src/item/Item.java
+++ src/item/Item.java
@@ -14,18 +14,21 @@ public abstract class Item extends Entity implements Interactable {
     private Sim owner;
     private String description;
     boolean keyItem;
-    public Item(String name, Sim owner, String description,boolean isKeyItem) throws InvalidEntityNameException {
+
+    public Item(String name, Sim owner, String description, boolean isKeyItem) throws InvalidEntityNameException {
         super(name);
         this.setOwner(owner);
         this.setDescription(description);
         this.setKeyItem(isKeyItem);
     }
-    public Item(String name, Sim owner,boolean isKeyItem) throws InvalidEntityNameException {
+
+    public Item(String name, Sim owner, boolean isKeyItem) throws InvalidEntityNameException {
         super(name);
         this.setOwner(owner);
         this.setDescription("");
         this.setKeyItem(isKeyItem);
     }
+
     public Item(Item sourceItem) throws InvalidEntityNameException {
         super(sourceItem.getName());
         this.setOwner(sourceItem.getOwner());
@@ -33,50 +36,64 @@ public abstract class Item extends Entity implements Interactable {
         this.setKeyItem(sourceItem.isKeyItem());
 
     }
+
     /**
      * @param intendedOwner - The sim the Item is trying to apply ownership to.
      */
     public void setOwner(Sim intendedOwner) {
-        if(hasOwner()){
+        if (hasOwner()) {
             this.owner = intendedOwner;
-        }
-        else {
+        } else {
             this.owner = null;
         }
     }
+
     //Removes any ownership of the item. Sets it to null.
-    public void unsetOwner(){
+    public void unsetOwner() {
         this.owner = null;
     }
 
     /**
      * description is optional. If String given to description is null for any reason, then an empty string will be given.
+     *
      * @param description - A String representing an Item's description.
      */
-    public void setDescription(String description){
+    public void setDescription(String description) {
         this.description = (description != null) ? description : "";
     }
+
     public void setKeyItem(boolean keyItem) {
         this.keyItem = keyItem;
     }
-    public Sim getOwner(){
+
+    public Sim getOwner() {
         return this.owner;
     }
-    public String getDescription(){
+
+    public String getDescription() {
         return this.description;
     }
+
     public boolean isKeyItem() {
         return this.keyItem;
     }
-    public boolean hasOwner(){
+
+    public boolean hasOwner() {
         return SimUtils.simExists(this.owner);
     }
-    public String toString(){
-        return "ID: " + super.getId() + "\n" +
-                "Owner" + this.getOwner().getName() + "\n" +
-                "Name: " + super.getName() + "\n" +
-                "Key Item: " + this.keyItemText() + "\n" +
-                "Description" + this.getDescription() + "\n";
+
+    public String toString() {
+        String string;
+        string = "ID: " + super.getId() + "\n";
+        if (this.hasOwner()) {
+            string += "Owner; " + this.getOwner().toString();
+        } else {
+            string += "Owner: none \n";
+        }
+        string += "Name: " + super.getName() + "\n";
+        string += "Key Item: " + this.keyItemText() + "\n";
+        string += "Description: " + this.getDescription() + "\n";
+        return string;
     }
 
     /**
