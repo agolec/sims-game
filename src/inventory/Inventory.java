0ad66f0e7160a965bdcb8a9f045e3bfb57eb2360
Adam Golec <algolec1990@yahoo.com>
2024-12-09 11:50:36 -0500

    create getItemByIndex and getItemIndex and itemInInventory methods.

--- src/inventory/Inventory.java
+++ src/inventory/Inventory.java
@@ -21,14 +21,23 @@ public class Inventory {
     public void addItem(Item item) {
         this.inventoryItems.add(item);
     }
-    public void removeOwner(Item item){
-        inventoryItems.remove(item);
+    public void removeOwner(String itemName,boolean removeFromInventory){
+        final int ITEM_NOT_FOUND = -1;
+        if(itemInInventory(itemName) && getItemIndex(itemName) != ITEM_NOT_FOUND){
+            this.inventoryItems.get(getItemIndex(itemName)).setOwner(null);
+            if(removeFromInventory){
+                System.out.println("Removing" + this.inventoryItems.get(getItemIndex(itemName)) + " from inventory.");
+                removeItemFromInventory(itemName);
+            }
+            System.out.println("Item successfully removed owner from inventory item.");
+        } else {
+            System.out.println("Item not in inventory.");
+        }
+    }
+    public void removeItemFromInventory(String itemName){
+        this.inventoryItems.remove(getItemIndex(itemName));
     }
 
-    /**
-     *
-     * @param item - Item desired to be removed from a sim's inventory.
-     */
 
     public int getInventorySize(){
         return this.inventoryItems.size();
@@ -41,12 +50,45 @@ public class Inventory {
             System.out.println((i + 1) + ". " + inventoryItems.get(i).getName());
         }
     }
+    public Item getItemByIndex(int i){
+        return this.inventoryItems.get(i);
+    }
+    public int getItemIndex(String itemName){
+        for(int i = 0; i < this.inventoryItems.size(); i++){
+            if(itemInInventory(itemName)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     *
+     * @param itemName - String of an item to search for in an inventory, ignoring case
+     * @return - true if present, false if not.
+     */
+    public boolean itemInInventory(String itemName){
+        for(Item item: this.inventoryItems){
+            if(item.getName().equalsIgnoreCase(itemName)){
+                return true;
+            }
+        }
+        return false;
+    }
     public String toString(){
         StringBuilder sb = new StringBuilder();
+        int i = 1;
         for(Item item : this.inventoryItems){
-            sb.append("-----");
+            sb.append("-----\n");
+            sb.append("Item ");
+            sb.append(i);
+            sb.append("\n");
             sb.append(item.toString());
-            sb.append("-----");
+            sb.append("-----\n");
+            i++;
+        }
+        if(sb.isEmpty()){
+            sb.append("no items in inventory.");
         }
         return sb.toString();
     }
