5592bf8443857835ec3f36035854e8810323b210
Adam Golec <algolec1990@yahoo.com>
2024-12-10 11:46:41 -0500

    set up utility method to return ownership of item sim may have.

--- src/utils/SimUtils.java
+++ src/utils/SimUtils.java
@@ -1,6 +1,7 @@
 package utils;
 
 import entities.Sim;
+import item.Item;
 
 /**
  * SimUtils is a utility to help readability with code that checks for existence of sims and other functions.
@@ -8,5 +9,8 @@ import entities.Sim;
 public class SimUtils {
     public static boolean simExists(Sim sim){
         return sim != null;
+    }
+    public static String getOwnerString(Item item) {
+        return item.hasOwner() ? "Owner: " + item.getOwner().toString() + "\n" : "Owner: none \n";
     }
 }
