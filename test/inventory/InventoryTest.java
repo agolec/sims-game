4f9d99ce125ffce216e213818dadad8c9fe207fd
Adam Golec <algolec1990@yahoo.com>
2024-12-10 11:47:18 -0500

    refactor unit tests for Inventory to account for new structure of Item class and Inventory Class.

--- test/inventory/InventoryTest.java
+++ test/inventory/InventoryTest.java
@@ -4,7 +4,6 @@ import entities.Sim;
 import exceptions.InvalidEntityNameException;
 import item.Appliance;
 import item.Item;
-import item.ItemType;
 import item.food.FoodItem;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,12 +13,10 @@ import java.util.ArrayList;
 
 public class InventoryTest {
     private Sim testSim;
-    private Inventory inventory;
     private ArrayList<Item> itemsSim1;
 
     @Before
     public void setUp() throws InvalidEntityNameException {
-        this.inventory = new Inventory();
         this.testSim = null;
         this.itemsSim1 = new ArrayList<>();
         try {
@@ -31,31 +28,24 @@ public class InventoryTest {
             System.out.println("Error: Failed to create sim: " + e.getMessage());
 
         }
+        Assert.assertNotNull("Failed: Sim should be created successfully at end of seUp(), and not-null, but it was found to be null.",testSim);
     }
 
     @Test
     public void testInventoryStartsEmpty() throws InvalidEntityNameException {
 
-        Assert.assertNotNull("Sim should be created successfully",testSim);
-        Assert.assertEquals("Inventory should be size 0 when Sim is created.",this.testSim.getInventory().getInventorySize(), 0);
-        for(int i = 0; i < this.itemsSim1.size(); i++){
-            System.out.println((i+1) + " " + this.itemsSim1.get(i).getName());
-        }
-        this.testSim.getInventory().addItem(this.itemsSim1.get(0));
-        Assert.assertEquals("Failed: Inventory should be size 1, after one item added following Inventory initialization.",1,this.testSim.getInventory().getInventorySize());
+        Assert.assertEquals("Failed: Inventory on " + this.testSim.getTypeName().toUpperCase() + " " + this.testSim.getName() +
+                                    "  should be size 0 when Sim is created.", this.testSim.getInventory().getInventorySize(), 0);
 
-        System.out.println(this.testSim.getInventory().toString());
 
     }
 
     @Test
     public void testInventoryIncreasesWhenItemAdded() {
-        /*Assert.assertNotNull(testSim,"Sim should be created successfully");
+        //Adding inventory to sim.
+        this.testSim.getInventory().addItem(this.itemsSim1.getFirst());
+        Assert.assertEquals("Failed: Inventory should be size 1, after one item added following Inventory initialization.",1,this.testSim.getInventory().getInventorySize());
 
-        Item dummyItem = new CoffAddeeMaker("Mr. Regular-Joe Coffee", ItemType.APPLIANCE,this.testSim,50.0,"Do you have a Sim who needs to burn the midnight oil? " +
-                "Keep them pumped full of coffee, and they'll spend less time " +
-                "napping and more time amusing you.",false);
-        Assert.assertEquals(this.testSim.getInventorySize(),0,"Inventory should be size 0 when Sim is created.");
-    }*/
+        System.out.println(this.testSim.getInventory().toString());
     }
 }
