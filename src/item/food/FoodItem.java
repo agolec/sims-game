ab3ff0fe239ea017de279fc0b54261aa84925f23
Adam Golec <algolec1990@yahoo.com>
2024-12-13 10:43:47 -0500

    move setters and getters to be grouped together by set or get method type.

--- src/item/food/FoodItem.java
+++ src/item/food/FoodItem.java
@@ -5,35 +5,45 @@ import exceptions.InvalidEntityNameException;
 import item.Item;
 
 public class FoodItem extends Item {
-    final int EMPTY_QUANTITY = 0;
-    int quantity;
+    final int NO_MORE_USES = 0;
+    int uses;
     int energyProvided;
 
-    public FoodItem(String name, Sim owner, String description, boolean isKeyItem,int quantity,int energyProvided) throws InvalidEntityNameException {
+    public FoodItem(String name, Sim owner, String description, boolean isKeyItem,int uses,int energyProvided) throws InvalidEntityNameException {
         super(name, owner, description, isKeyItem);
-        setQuantity(quantity);
+        setUses(uses);
         setEnergyProvided(energyProvided);
 
 
     }
     public FoodItem(FoodItem sourceItem) throws InvalidEntityNameException {
         super(sourceItem.getName(),sourceItem.getOwner(),sourceItem.getDescription(),sourceItem.isKeyItem());
-        this.setQuantity(sourceItem.getQuantity());
+        this.setUses(sourceItem.getUses());
         this.setEnergyProvided(sourceItem.getEnergyProvided());
     }
     public boolean isEmpty(){
-        return getQuantity() == EMPTY_QUANTITY;
+        return getUses() == NO_MORE_USES;
     }
-    public int getQuantity(){
-        return this.quantity;
+    public int getUses(){
+        return this.uses;
     }
     public int getEnergyProvided(){
         return this.energyProvided;
     }
-    public void setQuantity(int quantity){
-        this.quantity = quantity;
+    public void setUses(int uses){
+        this.uses = uses;
     }
     public void setEnergyProvided(int energyProvided){
         this.energyProvided = energyProvided;
+    }
+    public String toString(){
+        String usesLeftLabel = "Uses Left: ";
+        String energyProvidedLabel = "Energy Provided: ";
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(super.getString());
+        sb.append(usesLeftLabel).append(this.getUses()).append("\n");
+        sb.append(energyProvidedLabel).append(this.getEnergyProvided());
+        return sb.toString();
     }
 }
