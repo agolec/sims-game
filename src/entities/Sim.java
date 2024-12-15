884d08b074588c796a5b3fed38d54e052c414d0f
Adam Golec <algolec1990@yahoo.com>
2024-12-15 10:38:11 -0500

    set up Sim class to have Hunger need in order to test needs. Set up Sim class to have methods to return a need based on it's name, set needs, decay needs, set needs, set initial needs, and consume food items.

--- src/entities/Sim.java
+++ src/entities/Sim.java
@@ -4,19 +4,25 @@ import entities.base.Interactable;
 import exceptions.InvalidEntityNameException;
 import inventory.Inventory;
 import item.Item;
+import item.food.FoodItem;
 
 import java.util.ArrayList;
+import java.util.Collection;
 
 public class Sim extends Entity {
-
     private Inventory inventory;
+    private ArrayList<Need> needs;
     public Sim(String name) throws InvalidEntityNameException {
         super(name);
         this.inventory = new Inventory();
+        ArrayList<Need> needMap = new ArrayList<>();
+        needMap.add(new Need("Hunger",100,1));
+        setInitialNeeds(needMap);
     }
     public Sim(String name, ArrayList<Item> items) throws InvalidEntityNameException{
         super(name);
         this.inventory = new Inventory(items);
+        this.needs = new ArrayList<>();
     }
     public Sim(Sim sourceSim) throws InvalidEntityNameException{
         super(sourceSim.getName());
@@ -24,6 +30,51 @@ public class Sim extends Entity {
     public Inventory getInventory(){
         return this.inventory;
     }
+    public Need getNeed(String name){
+        for(Need need: this.needs){
+            if(need.getName().equalsIgnoreCase(name)){
+                return new Need(need);
+            }
+        }
+        return null;
+    }
+
+    public void consume(Item item){
+        if(item instanceof FoodItem == false){
+            System.out.println("Item is not food.");
+            return;
+        }
+        for(Need need: this.needs){
+            if(need.getName().equalsIgnoreCase("Hunger")){
+                need.replenish(((FoodItem) item).getEnergyProvided());
+                ((FoodItem) item).setUses(((FoodItem) item).getUses() - 1);
+            }
+        }
+    }
+    public void setInitialNeeds(ArrayList<Need> needs){
+        this.needs = new ArrayList<>();
+        for(Need need: needs){
+            this.needs.add(new Need(need.getName(),need.getValue(),need.getDecayRate()));
+        }
+    }
+    public void decayNeed(String name,int decayAmount,int decayQuantity){
+        for(Need need: this.needs){
+            if(need.getName().equalsIgnoreCase(name)){
+                need.decayNeed(decayAmount * decayQuantity);
+            }
+        }
+    }
+    public void setNeed(String name,int newValue){
+        for(Need need: this.needs){
+            if(need.getName().equalsIgnoreCase(name)){
+                need.replenish(newValue);
+                return;
+            }
+        }
+        System.out.println("Need not found.");
+    }
+
+
     public String toString(){
         StringBuilder sb = new StringBuilder();
         sb.append(super.getString());
