01a4d195229c9db2734c847eb06b48c43e66aa96
Adam Golec <algolec1990@yahoo.com>
2025-02-25 21:48:37 -0500

    set up unit tests for Menu class.

--- test/UI/MenuClassTests/Menu/MenuClassTests.java
+++ test/UI/MenuClassTests/Menu/MenuClassTests.java
@@ -1,2 +1,23 @@
-package UI.Menu;public class MenuClassTests {
+package UI.MenuClassTests.Menu;
+
+import UI.Menu;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MenuClassTests {
+    @Test
+    public void assertMenuLengthShouldEqualNumberOfOptions(){
+        List<String> neighborhoodMenuOptions = Arrays.asList("Create A Sim", "Evict","Exit Game");
+        Menu neighborhoodMenu = new Menu(neighborhoodMenuOptions);
+        Assert.assertEquals(neighborhoodMenuOptions,neighborhoodMenu.getOptions());
+    }
+    @Test
+    public void assertMenuEmpty(){
+        Menu menu = new Menu(new ArrayList<>());
+        Assert.assertTrue("Failure: Menu must be empty.",menu.getOptions().isEmpty());
+    }
 }
