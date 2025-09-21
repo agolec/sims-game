d51cb54c060d6488991a2732bc3eb4a2a7ab8540
Adam Golec <algolec1990@yahoo.com>
2025-09-21 14:13:29 -0400

    add getter/setter for broken and for dirty.

--- src/item/Appliance.java
+++ src/item/Appliance.java
@@ -11,12 +11,26 @@ public abstract class Appliance extends Item {
     private boolean broken;
     private boolean dirty;
     //relevent things to appliance class.
-    //private int appearanceModifier;
 
     public Appliance(String name, Sim owner, String description, boolean isKeyItem) throws InvalidEntityNameException {
         super(name, owner, description, isKeyItem);
 
     }
+
+    public boolean isBroken(){
+        return this.isBroken();
+    }
+    public boolean isDirty(){
+        return this.dirty;
+    }
+
+    public void setBroken(boolean broken){
+        this.broken = broken;
+    }
+    public void setDirty(boolean dirty) {
+        this.dirty = dirty;
+    }
+
 
     public String toString(){
         StringBuilder sb = new StringBuilder();
