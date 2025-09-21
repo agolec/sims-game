8bcbec01c427da33f05bc02f2b075c63e11d3313
Adam Golec <algolec1990@yahoo.com>
2025-09-21 13:46:32 -0400

    finalize Action interface for minimal MVP.

---
+++ src/item/action/Action.java
@@ -0,0 +1,9 @@
+package item.action;
+
+import entities.Entity;
+import entities.base.Interactable;
+import item.Item;
+
+public interface Action {
+    void perform(Entity actor, Interactable target);
+}
