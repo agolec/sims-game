a8a90aa1e1bb715c0c41e74eb2eae8f3e8b2e69c
Adam Golec <algolec1990@yahoo.com>
2025-09-21 13:49:47 -0400

    modify interact method in Interactable class.

--- src/entities/base/Interactable.java
+++ src/entities/base/Interactable.java
@@ -1,10 +1,11 @@
 package entities.base;
 
+import entities.Entity;
 import item.action.Action;
 
 import java.util.List;
 
 public interface Interactable {
     List<Action> getAvailableActions();
-
+    void interact(Entity actor, Action action);
 }
